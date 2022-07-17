d8963c710cbe6293358e5594471c7f6f127383a8
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-17 02:26:26 -0500

    new routes for new pages

--- routes.js
+++ routes.js
@@ -1,5 +1,15 @@
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes/index"),
+aboutRouter = require("./routes/about"),
+blogRouter = require("./routes/blog"),
+contactRouter = require("./routes/contact"),
+portfolioRouter = require("./routes/portfolio"),
+servicesRouter = require("./routes/services");
 
 module.exports = (app) => {
   app.use("/", indexRouter);
+  app.use("/sobre", aboutRouter);
+  app.use("/blog", blogRouter);
+  app.use("/contato", contactRouter);
+  app.use("/portfolio", portfolioRouter);
+  app.use("/servicos", servicesRouter);
 }
